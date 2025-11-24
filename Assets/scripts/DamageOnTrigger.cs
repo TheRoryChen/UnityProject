2bd59df70b3d4932882217349c6926ea3fdc9d2a
TheRoryChen <weiarnc@gmail.com>
2025-11-24 23:37:08 +0800

    Delete Assets directory

--- Assets/scripts/DamageOnTrigger.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class DamageOnTrigger: MonoBehaviour {
-    public int damage = 10; public string targetTag = "Enemy";
-    void OnTriggerEnter2D(Collider2D other){
-        if (other.CompareTag(targetTag)) other.GetComponent<Health>()?.Damage(damage);
-    }
-}
